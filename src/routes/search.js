3bdbd88a8961906d62fdbf5e48e14a2e996d58a8
Andrew Waterman <andrew.waterman@gmail.com>
2013-07-09 15:03:11 -0500

    Started returning just the actual search hits.

--- src/routes/search.js
+++ src/routes/search.js
@@ -5,8 +5,9 @@ var ElasticSearchClient = require('elasticsearchclient'),
     	port: 9200
 	},
     elasticSearchClient = new ElasticSearchClient(serverOptions);
+var _ = require('underscore');
 
-/* Search the stream */
+/* Search the stream with posted data */
 exports.search = function(req, res){
   var query = {
       "query":{
@@ -15,14 +16,34 @@ exports.search = function(req, res){
           }
       }
   }; 
-  var river = req.body.searchtype;
   var deferred = Q.defer();
-  var search = elasticSearchClient.search(river, 'jdbc', query);
-  search.on('data', function(data) { 
-      deferred.resolve(res.render('stream', {'data' : data}));
+  var search = elasticSearchClient.search(req.body.searchtype, 'jdbc', query);
+    
+  /* Fulfillment and error handling */
+  search.on('data', function(data) {
+      deferred.resolve(res.render('stream', {'data' : extract(data)}));
   });
+  
   search.on('error', function(error){
       deferred.reject(res.render(500,'Error'));
   });
-  search.exec();  
+    
+  /* Run our search */
+  search.exec();
+
+  /* Consolidation of JSON objects */
+  function extract(jsonStr) {
+      try {
+      	var data = JSON.parse(jsonStr);
+        var hits = data.hits.hits;
+      	/* Use underscore to consolidate the objects */
+        console.log("total hits: " + data.hits.total);
+        console.log("array size: " + hits.length);  
+
+      } catch (Exception) {
+      	console.log("Error in conosolidation method!");
+      }
+      
+      return JSON.stringify(hits);
+  }
 };
