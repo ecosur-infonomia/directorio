86fd8da10e0377b32dc7cf3e7056e27c0b1b3d63
Andrew Waterman <andrew.waterman@gmail.com>
2013-07-09 13:33:46 -0500

    Working with pre display of json.

--- routes/search.js
+++ routes/search.js
@@ -1,9 +1,9 @@
-ElasticSearchClient = require('elasticsearchclient');
-var serverOptions = {
-    host: 'localhost',
-    port: 9200
-};
-var elasticSearchClient = new ElasticSearchClient(serverOptions);
+var ElasticSearchClient = require('elasticsearchclient'),
+    serverOptions = {
+    	host: 'localhost',
+    	port: 9200
+	},
+    elasticSearchClient = new ElasticSearchClient(serverOptions);
 
 /* Search the stream */
 exports.search = function(req, res){
@@ -17,11 +17,9 @@ exports.search = function(req, res){
   var river = req.body.searchtype;
   elasticSearchClient.search(river, 'jdbc', query)
     .on('data', function(data) {
-        console.log("results from " + river);
         res.render('stream', {'data' : data});
     })
     .on('error', function(error){
-        console.log(error);
         res.render(500,'Error');
     })
     .exec()  
