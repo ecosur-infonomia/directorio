4d1ea1cbb038473faca3e5fbadfe0fafccb70022
Andrew Waterman <andrew.waterman@gmail.com>
2013-07-19 13:33:55 -0500

    Added in domains for stability and error tracing support.

--- src/routes/index.js
+++ src/routes/index.js
@@ -7,46 +7,55 @@ var Elastic = require('elasticsearchclient'),
        port: 9200
    },
    client = new Elastic(serverOptions);
+var domain = require('domain');
 
 /* 
  * The index function populates and returns the index page 
  */
 exports.index = function(req, res){
-  var current = (!req.query.page) ? 10 : parseInt(req.query.page);
-  var size = (!req.query.size) ? 10 : parseInt(req.query.size);
+  /* Domains enable easy error catching and keep the server from 
+     crashing */
+  var d = domain.create();
+  d.on("error", function(error) {
+      console.log("Error!", error);
+  });
+  d.add(req);
+  d.add(res);
+  d.run(function() {
+    var current = (!req.query.page) ? 10 : parseInt(req.query.page);
+    var size = (!req.query.size) ? 10 : parseInt(req.query.size);
 
-   /* Get all the regimenes names from Admin DB, if this 
-      increases beyond 100 in the future, the size parameter
-      must be increased as well. */
-   var query = {
-      "from": 0,
-  	  "size": 100,      
-      "query":{
-          "query_string":{
-              "query" : "*"
-          }
-      },
-      "sort" : [
-        { "NO10_NOMBRE" : { "order" : "asc", "mode" : "avg"} }
-      ]
-   };
-   
-   var promise = Q.defer();
-   var search = client.search('reg', 'jdbc', query);
+     /* Get all the regimenes names from Admin DB, if this 
+        increases beyond 100 in the future, the size parameter
+        must be increased as well. */
+     var query = {
+        "from": 0,
+        "size": 100,      
+        "query":{
+            "query_string":{
+                "query" : "*"
+            }
+        },
+        "sort" : [
+          { "NO10_NOMBRE" : { "order" : "asc", "mode" : "avg"} }
+        ]
+     };
+     
+     var promise = Q.defer();
+     var search = client.search('reg', 'jdbc', query);
+      
+     /* Fulfillment and error handling */
+     search.on('data', function(jsonStr) {
+        var data = JSON.parse(jsonStr);
+        var hits = data.hits.hits;
+        promise.resolve(res.render('index', {
+            'title': 'Directorio Interna de ECOSUR',
+            'data' : hits}));
+     });
     
-   /* Fulfillment and error handling */
-   search.on('data', function(jsonStr) {
-      var data = JSON.parse(jsonStr);
-      var hits = data.hits.hits;
-      promise.resolve(res.render('index', {
-          'title': 'Directorio Interna de ECOSUR',
-          'data' : hits}));
-   });
-  
-   search.on('error', function(error){
-      promise.reject(res.render(500, error));
-   });
-    
-   /* Run our search */
-   search.exec();
-};
+     search.on('error', function(error){
+        promise.reject(res.render(500, error));
+     });
+     /* Run our search */
+     search.exec();
+  })};
--- src/routes/search.js
+++ src/routes/search.js
@@ -5,120 +5,133 @@ var ElasticSearchClient = require('elasticsearchclient'),
     	port: 9200
 	},
     elasticSearchClient = new ElasticSearchClient(serverOptions);
+var domain = require('domain');
 
 /* Perform a search */
 exports.search = function(req, res){
-  var size = req.body.size || 10,
-      current = req.body.from || 0;
-  var query_string = {
-      "from": current,
-  	  "size": size,
-      "query":{
-          "query_string":{
-              "query" : req.body.phrase
-          }
-      },
-      "sort" : [
-        "_score",
-        { "NO01_APELLIDO_PAT" : {"missing" : "_last"} }
-    ]
-  }; 
-  var promise = Q.defer();
-  var search = elasticSearchClient.search('admin', 'jdbc', query_string);
-  
-  /* error callback */
-  search.on('error', function(error){
-      promise.reject(res.status(500).send('Error'));
+  var d = domain.create();
+  d.on("error", function(error) {
+      console.log("Domain Error!", error);
   });
-
-  /* data callback */
-  search.on('data', function(jsonStr) {
-      var data = JSON.parse(jsonStr);
-      var hits = data.hits.hits;
-      
-      /* Create the page list to pass on to the view engine 
-        for individual page numbering if desired */
-      var pages = [];
-      var number = (data.hits.total) / size;
-      if (number > 1) {
-      	for (var i = 0; i < number; i++) {
-      		pages.push({"page" : i + 1 });
-      	}
-      }
-
-      var back, next;
-      (current >= size) ? back = current - size : back = 0;
-      next = current + size;
-      
-      promise.resolve(res.render('admin', {
-          'query' : req.body.phrase, 
-          'data'  : hits,	// Returned search hits
-          'size'  : size,   // Number of pages to return per call
-          'pages' : pages,  // Number of pages total in the search
-          'back'  : back,   // from parameter for back 
-          'next'  : next    // from parameter for next
-      }));
-  });
+  d.add(req);
+  d.add(res);
+  d.run(function() {
+    var size = req.body.size || 10,
+        current = req.body.from || 0;
+    var query_string = {
+        "from": current,
+        "size": size,
+        "query":{
+            "query_string":{
+                "query" : req.body.phrase
+            }
+        },
+        "sort" : [
+          "_score",
+          { "NO01_APELLIDO_PAT" : {"missing" : "_last"} }
+      ]
+    }; 
+    var promise = Q.defer();
+    var search = elasticSearchClient.search('admin', 'jdbc', query_string);
     
-  /* Execute the search */
-  search.exec();
+    /* error callback */
+    search.on('error', function(error){
+        promise.reject(res.status(500).send('Error'));
+    });
 
-};
+    /* data callback */
+    search.on('data', function(jsonStr) {
+        var data = JSON.parse(jsonStr);
+        var hits = data.hits.hits;
+        
+        /* Create the page list to pass on to the view engine 
+          for individual page numbering if desired */
+        var pages = [];
+        var number = (data.hits.total) / size;
+        if (number > 1) {
+          for (var i = 0; i < number; i++) {
+            pages.push({"page" : i + 1 });
+          }
+        }
+
+        var back, next;
+        (current >= size) ? back = current - size : back = 0;
+        next = current + size;
+        
+        promise.resolve(res.render('admin', {
+            'query' : req.body.phrase, 
+            'data'  : hits, // Returned search hits
+            'size'  : size,   // Number of pages to return per call
+            'pages' : pages,  // Number of pages total in the search
+            'back'  : back,   // from parameter for back 
+            'next'  : next    // from parameter for next
+        }));
+    });      
+    /* Execute the search */
+    search.exec();
+
+  })};
 
 /* Page through a search stream */
 exports.page = function(req, res) {
+  var d = domain.create();
+  d.on("error", function(error) {
+      console.log("Domain Error!", error);
+  });
+  d.add(req);
+  d.add(res);
+  d.run(function() {
     var current = parseInt(req.query.page);
     var size = parseInt(req.query.size);
 
+    /* Paged query has different sort than POST in terms of score result */
     var query_string =  {
-      "from": current,
-      "size": size,
-      "query":{
-        "query_string": { "query": req.query.query }
-      },
-      "sort" : [
-        "_score",
-        { "NO01_APELLIDO_PAT" : {"missing" : "_last"} }
-      ]
-    }
-
-  var promise = Q.defer();
-  var search = elasticSearchClient.search('admin', 'jdbc', query_string);
-
-  /* error callback */
-  search.on('error', function(error){
-      promise.reject(res.status(500).send('Error'));
-  });
-
-  /* data callback */
-  search.on('data', function(jsonStr) {
-      var data = JSON.parse(jsonStr);
-      var hits = data.hits.hits;
-      
-      /* Create the page list to pass on to the view engine 
-        for individual page numbering if desired */
-      var pages = [];
-      var number = (data.hits.total) / size;
-      if (number > 1) {
-        for (var i = 0; i < number; i++) {
-          pages.push({"page" : i + 1 });
-        }
+        "from": current,
+        "size": size,
+        "query":{
+          "query_string": { "query": req.query.query }
+        },
+        "sort" : [
+          { "NO01_APELLIDO_PAT" : {"missing" : "_last"} },
+        "_score"
+        ]
       }
 
-      var back, next;
-      (current >= size) ? back = current - size : back = 0;
-      next = current + size;
-      
-      promise.resolve(res.render('admin', {
-          'query' : req.query.query, 
-          'data'  : hits,              // Returned search hits
-          'size'  : size,              // Number of pages to return per call
-          'pages' : pages,             // Number of pages total in the search
-          'back'  : back,              // from parameter for back 
-          'next'  : next               // from parameter for next
-      }));
-  });
-  
-  search.exec(); // Run the next query to ES 
-  
-};
+    var promise = Q.defer();
+    var search = elasticSearchClient.search('admin', 'jdbc', query_string);
+
+    /* error callback */
+    search.on('error', function(error){
+        promise.reject(res.status(500).send('Error'));
+    });
+
+    /* data callback */
+    search.on('data', function(jsonStr) {
+        var data = JSON.parse(jsonStr);
+        var hits = data.hits.hits;
+        
+        /* Create the page list to pass on to the view engine 
+          for individual page numbering if desired */
+        var pages = [];
+        var number = (data.hits.total) / size;
+        if (number > 1) {
+          for (var i = 0; i < number; i++) {
+            pages.push({"page" : i + 1 });
+          }
+        }
+
+        var back, next;
+        (current >= size) ? back = current - size : back = 0;
+        next = current + size;
+        
+        promise.resolve(res.render('admin', {
+            'query' : req.query.query, 
+            'data'  : hits,              // Returned search hits
+            'size'  : size,              // Number of pages to return per call
+            'pages' : pages,             // Number of pages total in the search
+            'back'  : back,              // from parameter for back 
+            'next'  : next               // from parameter for next
+        }));
+    });
+    search.exec(); // Run the next query to ES 
+  })};
